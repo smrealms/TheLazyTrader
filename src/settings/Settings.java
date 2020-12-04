e796fdb5f630877a3661880c58a7c96aac22e037
Jerome Doby <xaerodegreaz@gmail.com>
2020-12-05 04:57:32 +0900

    use maven to build project use maven to stamp artifact version, and supply it to title, and about text add launch4j build step

--- src/settings/Settings.java
+++ src/settings/Settings.java
@@ -8,8 +8,7 @@ public class Settings
 	public final static int TURNS_WARP_SECTOR_EQUIVALENCE = (int)Math.round(TURNS_PER_WARP-TURNS_PER_SECTOR/TURNS_PER_SECTOR);
 	public final static double TURNS_PER_TRADE = 1;// + 0.4/* Maintenance */+ 0.3/* Mercs */;
 //	public final static int DEFAULT_DISTANCE_LIMIT = 100;
-	public final static String VERSION = "1.3.8";
-	public static final String ABOUT = "Route generation and general tools for SMR 1.6. (<a href=\"http://www.smrealms.de\">http://www.smrealms.de</a>)<br />" + "v" + VERSION + "<br /><br />" + "Created by Page";
+	public static final String ABOUT = "Route generation and general tools for SMR 1.6. (<a href=\"http://www.smrealms.de\">http://www.smrealms.de</a>)<br />" + "v%s<br /><br />" + "Created by Page";
 //	public static final int DEFAULT_TRIM_TO_BEST_X_ROUTES = 200;
 	public static final int DEFAULT_RACE_RELATIONS = 0;
 	public static final boolean DEFAULT_USE_RELATIONS_FACTOR = true;
